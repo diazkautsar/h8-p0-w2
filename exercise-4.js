a7853b0683cbb07e2fef01c52abeed7e95db4806
Diaz Kautsar <diazkautsar77@gmail.com>
2019-10-22 19:08:20 +0700

    exercise hari selasa

--- exercise-4.js
+++ exercise-4.js
@@ -1,13 +1,13 @@
-var tanggal = 31;
-var bulan = 4;
-var tahun = 2300;
+var tanggal = 30;
+var bulan = 2;
+var tahun = 2020;
 var pesan = '';
 var cetak;
 
 // Validasi Bulan berjumlah 31.
 if (bulan === 1 || bulan === 3 || bulan === 5 || bulan === 7 || bulan === 8 || bulan === 10 || bulan === 12) {
     if (tanggal < 1 || tanggal > 31) {
-        pesan += 'Tanggal Salah BOS\n'
+        pesan += 'Revisi tanggal. Tanggal tidak boleh kurang dari 1 dan tidak boleh lebih dari 31 karena ini bulan berjumlah 31 hari\n'
     } else {
         switch (bulan) {
             case 1:
@@ -37,7 +37,7 @@ if (bulan === 1 || bulan === 3 || bulan === 5 || bulan === 7 || bulan === 8 || b
 // Validasi Bulan Berjumlah 30
 if (bulan === 4 || bulan === 6 || bulan === 9 || bulan === 11) {
     if (tanggal < 1 || tanggal > 30) {
-        pesan += 'anda PUSING?\n'
+        pesan += 'Revisi tanggal. Tanggal tidak boleh kurang dari 1 dan tidak boleh lebih dari 30 karena ini berjumlah 30 hari\n'
     } else {
         switch (bulan) {
             case 4:
@@ -59,7 +59,7 @@ if (bulan === 4 || bulan === 6 || bulan === 9 || bulan === 11) {
 if (bulan === 2) {
     if ((tahun % 4 === 0 && tahun % 100 !== 0) || (tahun % 4 === 0 && tahun % 100 === 0)) {
         if (tanggal < 1 || tanggal > 29) {
-            pesan += 'Kekurangan atau Kelebihan\n';
+            pesan += 'Revisi tangal. Tanggal tidak boleh kurang dari satu dan tidak boleh lebih dari 29 karena ini Bulan Februari di Tahun Kabisat\n';
         } else {
             switch (bulan) {
                 case 2:
@@ -69,7 +69,7 @@ if (bulan === 2) {
         } 
     } else {
         if (tanggal < 1 || tanggal > 28) {
-            pesan += 'Kekurangan braaayy\n';
+            pesan += 'Revisi tangal. Tanggal tidak boleh kurang dari satu dan tidak boleh lebih dari 28 karena ini Bulan Februari berjumlah 28 hari\n';
         } else {
             switch (bulan) {
                 case 2:
@@ -81,7 +81,7 @@ if (bulan === 2) {
 }
 // Validasi tahun 1900 - 2200
 if (tahun < 1900 || tahun > 2200) {
-    pesan += 'Salah Tahun BOS';
+    pesan += 'Revisi tahun. Tahun tidak boleh kurang dari 1900 dan tidak lebih dari 2200';
 }
 
 
